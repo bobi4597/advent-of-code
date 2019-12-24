9b4ddc4d6e501afefa1289c72e92b1d6c0f789cb
Bozidar Ilievski <bobi1978@gmail.com>
2019-12-24 23:57:16 +0100

    Add solution for Advent of Code 2019, Day 24, Part 2

--- src/main/java/com/github/bobi4597/adventofcode/Year2019Day24.java
+++ src/main/java/com/github/bobi4597/adventofcode/Year2019Day24.java
@@ -5,14 +5,60 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class Year2019Day24 {
-    static int N = 5;
-    static int startPosition;
+
+    private static int N = 5;
+    private static int startPosition;
     private static int[] di = {-1, 0, 0, 1};
     private static int[] dj = {0, -1, 1, 0};
+
+    private static int N2 = 203;
+    private static int MINUTES = 200;
+    private static int[][][] a = new int[N2][N][N], b = new int[N2][N][N];
+
+    // neighbours for part 2 :-). Each element is: [delta level, i, j]
+    private static int[][][][] d = new int[5][5][][];
+    static {
+        // outer circle
+        d[0][0] = new int[][] {{0, 0, 1},            {0, 1, 0}, {-1, 2, 1}, {-1, 1, 2}};
+        d[0][1] = new int[][] {{0, 0, 0}, {0, 0, 2}, {0, 1, 1},             {-1, 1, 2}};
+        d[0][2] = new int[][] {{0, 0, 1}, {0, 0, 3}, {0, 1, 2},             {-1, 1, 2}};
+        d[0][3] = new int[][] {{0, 0, 2}, {0, 0, 4}, {0, 1, 3},             {-1, 1, 2}};
+        d[0][4] = new int[][] {{0, 0, 3},            {0, 1, 4}, {-1, 2, 3}, {-1, 1, 2}};
+
+        d[1][0] = new int[][] {{0, 0, 0}, {0, 2, 0}, {0, 1, 1}, {-1, 2, 1}};
+        d[2][0] = new int[][] {{0, 1, 0}, {0, 3, 0}, {0, 2, 1}, {-1, 2, 1}};
+        d[3][0] = new int[][] {{0, 2, 0}, {0, 4, 0}, {0, 3, 1}, {-1, 2, 1}};
+
+        d[1][4] = new int[][] {{0, 0, 4}, {0, 2, 4}, {0, 1, 3}, {-1, 2, 3}};
+        d[2][4] = new int[][] {{0, 1, 4}, {0, 3, 4}, {0, 2, 3}, {-1, 2, 3}};
+        d[3][4] = new int[][] {{0, 2, 4}, {0, 4, 4}, {0, 3, 3}, {-1, 2, 3}};
+
+        d[4][0] = new int[][] {{0, 4, 1},            {0, 3, 0}, {-1, 2, 1}, {-1, 3, 2}};
+        d[4][1] = new int[][] {{0, 4, 0}, {0, 4, 2}, {0, 3, 1},             {-1, 3, 2}};
+        d[4][2] = new int[][] {{0, 4, 1}, {0, 4, 3}, {0, 3, 2},             {-1, 3, 2}};
+        d[4][3] = new int[][] {{0, 4, 2}, {0, 4, 4}, {0, 3, 3},             {-1, 3, 2}};
+        d[4][4] = new int[][] {{0, 4, 3},            {0, 3, 4}, {-1, 2, 3}, {-1, 3, 2}};
+
+        // inner circle (corners)
+        d[1][1] = new int [][] {{0, 0, 1}, {0, 2, 1}, {0, 1, 0}, {0, 1, 2}};
+        d[1][3] = new int [][] {{0, 0, 3}, {0, 2, 3}, {0, 1, 2}, {0, 1, 4}};
+        d[3][1] = new int [][] {{0, 2, 1}, {0, 4, 1}, {0, 3, 0}, {0, 3, 2}};
+        d[3][3] = new int [][] {{0, 2, 3}, {0, 4, 3}, {0, 3, 2}, {0, 3, 4}};
+
+        // inner circle (not corners)
+        d[1][2] = new int[][] {{0, 1, 1}, {0, 1, 3}, {0, 0, 2},   {1, 0, 0}, {1, 0, 1}, {1, 0, 2}, {1, 0, 3}, {1, 0, 4}};
+        d[3][2] = new int[][] {{0, 3, 1}, {0, 3, 3}, {0, 4, 2},   {1, 4, 0}, {1, 4, 1}, {1, 4, 2}, {1, 4, 3}, {1, 4, 4}};
+        d[2][1] = new int[][] {{0, 1, 1}, {0, 3, 1}, {0, 2, 0},   {1, 0, 0}, {1, 1, 0}, {1, 2, 0}, {1, 3, 0}, {1, 4, 0}};
+        d[2][3] = new int[][] {{0, 1, 3}, {0, 3, 3}, {0, 2, 4},   {1, 0, 4}, {1, 1, 4}, {1, 2, 4}, {1, 3, 4}, {1, 4, 4}};
+
+        // middle point
+        d[2][2] = new int[][] {};
+    }
 
     public static void main(String[] args) {
         readInput();
         System.out.printf("%d\n", solve1());
+        System.out.printf("%d\n", solve2());
     }
 
     private static void readInput() {
@@ -24,12 +70,12 @@ public class Year2019Day24 {
             String line = scanner.nextLine();
             for (int j = 0; j < 5; ++j) {
                 if (line.charAt(j) == '#') {
+                    a[N2 / 2][i][j] = 1;
                     startPosition += pow;
                 }
                 pow *= 2;
             }
         }
-        System.out.println(startPosition);
     }
 
     //////////////// part 1 /////////////////
@@ -93,5 +139,80 @@ public class Year2019Day24 {
             }
         }
         System.out.println();
+    }
+
+    ////////////// part 2 //////////////////
+    private static int solve2() {
+        for (int t = 0; t < MINUTES; ++t) {
+            for (int level = 1; level < N2 - 1; ++level) {
+                for (int i = 0; i < N; ++i) {
+                    for (int j = 0; j < N; ++j) {
+                        int[][] neighbours = d[i][j];
+                        int bugs = 0;
+                        for (int k = 0; k < neighbours.length; ++k) {
+                            int deltaLevel = neighbours[k][0];
+                            int ni = neighbours[k][1];
+                            int nj = neighbours[k][2];
+                            if (a[level + deltaLevel][ni][nj] == 1) {
+                                ++bugs;
+                            }
+                        }
+
+                        if (a[level][i][j] == 1) {
+                            if (bugs == 1) {
+                                // bug -> bug: 1 bug around the bug
+                                b[level][i][j] = 1;
+                            } else {
+                                // bug -> empty: 0, 2, 3 bugs around the bug
+                                b[level][i][j] = 0;
+                            }
+                        } else {
+                            if (bugs == 1 || bugs == 2) {
+                                // empty -> bug: 1 or 2 bugs around the empty
+                                b[level][i][j] = 1;
+                            } else {
+                                // empty -> empty: 0 or 3 bugs around the empty
+                                b[level][i][j] = 0;
+                            }
+                        }
+                    }
+                }
+            }
+            // move b -> a, and clear b
+            for (int level = 1; level < N2 - 1; ++level) {
+                for (int i = 0; i < N; ++i) {
+                    for (int j = 0; j < N; ++j) {
+                        a[level][i][j] = b[level][i][j];
+                        b[level][i][j] = 0;
+                    }
+                }
+            }
+        }
+        // count bugs
+        int bugs = 0;
+        for (int level = 1; level < N2 - 1; ++level) {
+            for (int i = 0; i < N; ++i) {
+                for (int j = 0; j < N; ++j) {
+                    if (a[level][i][j] == 1) {
+                        ++bugs;
+                    }
+                }
+            }
+        }
+        //printA();
+        return bugs;
+    }
+
+    private static void printA() {
+        for (int level = 0; level < N2; ++level) {
+            for (int i = 0; i < N; ++i) {
+                for (int j = 0; j < N; ++j) {
+                    System.out.printf("%s", a[level][i][j] == 1 ? "#" : ".");
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+
     }
 }
