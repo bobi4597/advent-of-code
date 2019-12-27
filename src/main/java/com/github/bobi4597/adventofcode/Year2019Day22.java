89160b2f2a3c8546dbdee6372f957d3386296240
Bozidar Ilievski <bobi1978@gmail.com>
2019-12-27 21:07:30 +0100

    Add solution for Advent of Code 2019, Day 22, Part 2

--- src/main/java/com/github/bobi4597/adventofcode/Year2019Day22.java
+++ src/main/java/com/github/bobi4597/adventofcode/Year2019Day22.java
@@ -1,63 +1,144 @@
 package com.github.bobi4597.adventofcode;
 
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Year2019Day22 {
 
-    private static final int LENGTH = 10_007;
-    private static final int INDEX1 = 2019;
+    private static final long LENGTH = 10_007L;
+    private static final long INDEX1 = 2019L;
+
+    private static final long LENGTH2 = 119315717514047L;
+    private static final long POWER2 = 101741582076661L;
+    private static final long INDEX2 = 2020L;
+
 
     public static void main(String[] args) {
-        List<int[]> deals = readInput1(LENGTH);
-        System.out.printf("%d\n", solve11(LENGTH, deals));
+//        List<BigInteger[]> deals = readInput1(LENGTH);
+//        System.out.printf("%s\n", solve1(LENGTH, deals));
+        List<BigInteger[]> deals = readInput2(LENGTH2);
+        System.out.printf("%s\n", solve2(LENGTH2, deals));
     }
 
-    private static int solve11(int L, List<int[]> deals) {
-        int[] f1 = new int[]{1, 0};
-        for (int[] f2 : deals) {
-            f1 = composition(f2, f1, L);
+    ///////////////////////////////////////////////////////////
+    /////////////////////// part 1 ////////////////////////////
+    ///////////////////////////////////////////////////////////
+
+    private static String solve2(long L, List<BigInteger[]> deals) {
+        BigInteger[] f1 = new BigInteger[]{BigInteger.ONE, BigInteger.ZERO};
+        for (int i = deals.size() - 1; i >= 0; --i) {
+            BigInteger[] f2 = deals.get(i);
+            f1 = composition2(f2, f1, L);
         }
-        int y = ((f1[0] * INDEX1 + f1[1]) % L + L) % L;
-        return y;
+
+
+        // f1 is the function after applying all the deals from the input
+        // now we have to shuffle POWER2 times
+        BigInteger[] f = pow(f1, POWER2, LENGTH2);
+
+        BigInteger bigL = BigInteger.valueOf(L);
+        BigInteger index2 = BigInteger.valueOf(INDEX2);
+        BigInteger y = f[0].multiply(index2).add(f[1]).remainder(bigL).add(bigL).remainder(bigL);
+        return y.toString();
     }
 
-    private static int[] dealIntoNewStack1(int L) {
-        return new int[]{-1, L - 1};
+    private static BigInteger[] dealIntoNewStack2(long L) {
+        return new BigInteger[]{BigInteger.ONE.negate(), BigInteger.valueOf(L - 1)};
     }
 
-    private static int[] cut1(int n) {
-        return new int[]{1, -n};
+    private static BigInteger[] cut2(long n) {
+        return new BigInteger[]{BigInteger.ONE, BigInteger.valueOf(n).negate()};
     }
 
-    private static int[] dealWithIncrement1(int n) {
-        return new int[]{n, 0};
+    private static BigInteger[] dealWithIncrement2(long n) {
+        return new BigInteger[]{BigInteger.valueOf(n), BigInteger.ZERO};
     }
 
-    private static int[] composition(int[] f2, int[] f1, int L) {
-        return new int[]{(f1[0] * f2[0]) % L, (f2[0] * f1[1] + f2[1]) % L};
+    private static BigInteger[] composition2(BigInteger[] f2, BigInteger[] f1, long L) {
+        BigInteger bigL = BigInteger.valueOf(L);
+        return new BigInteger[]{
+            f1[0].multiply(f2[0]).remainder(bigL),
+            f2[0].multiply(f1[1]).add(f2[1]).remainder(bigL)
+        };
     }
 
-    private static List<int[]> readInput1(int L) {
-        List<int[]> f = new ArrayList<>();
+    /**
+     * Calculate f^p(x).
+     * f^p = {
+     * p % 2 == 0: (f^2)^(p / 2)
+     * p % 2 == 1: f * f^(p - 1)
+     * }
+     */
+    private static BigInteger[] pow(BigInteger[] f, long p, long L) {
+        if (p == 1) return f;
+        if (p % 2 == 0) {
+            return pow(composition2(f, f, L), p / 2, L);
+        } else {
+            return composition2(f, pow(f, p - 1, L), L);
+        }
+    }
+
+    private static List<BigInteger[]> readInput2(long L) {
+        BigInteger bigL = BigInteger.valueOf(L);
+        List<BigInteger[]> f = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
         while (scanner.hasNextLine()) {
             String input = scanner.nextLine();
             if (input.startsWith("deal into new stack")) {
-                f.add(dealIntoNewStack1(L));
+                f.add(dealIntoNewStack2(L));
             } else if (input.startsWith("cut")) {
-                int n = Integer.parseInt(input.substring(4));
-                if (n < 0) {
+                long n = Long.parseLong(input.substring(4));
+                if (n < 0L) {
                     n = L + n;
                 }
-                f.add(cut1(n));
+                n = L - n;
+                f.add(cut2(n));
             } else {
-                int n = Integer.parseInt(input.substring(20));
-                f.add(dealWithIncrement1(n));
+                long n = Long.parseLong(input.substring(20));
+                BigInteger modInverse = BigInteger.valueOf(n).modInverse(bigL);
+                long invN = modInverse.longValue();
+                f.add(dealWithIncrement2(invN));
             }
         }
         return f;
     }
 
+    ///////////////////////////////////////////////////////////
+    /////////////////////// part 1 ////////////////////////////
+    ///////////////////////////////////////////////////////////
+
+    private static String solve1(long L, List<BigInteger[]> deals) {
+        BigInteger[] f1 = new BigInteger[]{BigInteger.ONE, BigInteger.ZERO};
+        for (BigInteger[] f2 : deals) {
+            f1 = composition2(f2, f1, L);
+        }
+        BigInteger bigL = BigInteger.valueOf(L);
+        BigInteger startIndex = BigInteger.valueOf(INDEX1);
+        BigInteger endIndex = f1[0].multiply(startIndex).add(f1[1]).remainder(bigL).add(bigL).remainder(bigL);
+        return endIndex.toString();
+    }
+
+    private static List<BigInteger[]> readInput1(long L) {
+        BigInteger bigL = BigInteger.valueOf(L);
+        List<BigInteger[]> f = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            String input = scanner.nextLine();
+            if (input.startsWith("deal into new stack")) {
+                f.add(dealIntoNewStack2(L));
+            } else if (input.startsWith("cut")) {
+                long n = Long.parseLong(input.substring(4));
+                if (n < 0L) {
+                    n = L + n;
+                }
+                f.add(cut2(n));
+            } else {
+                long n = Long.parseLong(input.substring(20));
+                f.add(dealWithIncrement2(n));
+            }
+        }
+        return f;
+    }
 }
