ed204f3cd63375be1171a95e7a5ec25e007b87f4
Bozidar Ilievski <bobi1978@gmail.com>
2019-12-28 19:19:07 +0100

    Add solution for Advent of Code 2019, Day 9, Part 2

--- src/main/java/com/github/bobi4597/adventofcode/Year2019Day9.java
+++ src/main/java/com/github/bobi4597/adventofcode/Year2019Day9.java
@@ -7,11 +7,18 @@ public class Year2019Day9 {
     public static void main(String[] args) {
         String inputString = readInput();
         System.out.printf("Part 1: %s\n", solve1(inputString));
+        System.out.printf("Part 2: %s\n", solve2(inputString));
     }
 
     private static String solve1(String inputString) {
         BigIntCodeComputer a = new BigIntCodeComputer(inputString);
         BigRunResult runResult = a.run(new int[] {1});
+        return runResult.output.toString();
+    }
+
+    private static String solve2(String inputString) {
+        BigIntCodeComputer a = new BigIntCodeComputer(inputString);
+        BigRunResult runResult = a.run(new int[] {2});
         return runResult.output.toString();
     }
 }
